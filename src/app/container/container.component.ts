6943cef7906777e9d94deb160b56904e1f338a9e
sahil-2411 <sahil9920667195@gmail.com>
2024-06-18 21:41:33 +0530

    done till ngIf search bar

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+    // name: String = "Iphone";
+    // price: number = 999;
+    // color: String = "Red";
+    //  name="John Doe";
+
+    // addToCart: number = 0;
+    // product={
+    //   name :"Iphone",
+    //   price  :  999,
+    //   color :  "Red",
+    //   discount : 8.5,
+    //   instock : 10,
+    //   pimage: "/assets/iphone.png"
+    // }
+
+    // getDiscount(){
+    //   return this.product.price - (this.product.price * this.product.discount/100);
+    // }
+
+//     onNameChange(event: any){
+//       this.name = event.target.value;
+//     }
+
+    // decrementCartVlaue(){
+    //   if(this.addToCart>0)
+    //   this.addToCart--;
+    // }
+
+    // incrementCartVlaue(){
+    //   if(this.addToCart<this.product.instock)
+    //   this.addToCart++;
+    // }
+
+
+    // everything above was just for practice
+
+    //listOfString: string[]= ["marse","jupiter","earth"];
+ }
