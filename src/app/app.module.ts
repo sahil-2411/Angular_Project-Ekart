31c41e4035ee8808845a09fa592c3bcbaddcb3f6
sahil-2411 <sahil9920667195@gmail.com>
2024-07-06 23:13:30 +0530

    added offers elemrnt at bottom of page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { TopheaderComponent } from './topheader/topheader.component';
+import { FeaturedBrandsComponent } from './container/featured-brands/featured-brands.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { TopheaderComponent } from './topheader/topheader.component';
     ProductListComponent,
     ProductComponent,
     FilterComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    FeaturedBrandsComponent
   ],
   imports: [
     BrowserModule,
